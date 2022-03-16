db817bcdffe01471b05f67355a9e1b5870cd6e82
“PrinceXxShadows” <princexxshadows@gmail.com>
2022-03-16 09:03:01 -0500

    excercise complete

---
+++ script.js
@@ -0,0 +1,27 @@
+for (i = 1; i <=7; i++){
+    console.log(i)
+};
+
+console.log(`============`);
+
+for (i = 5; i <= 25; i += 4);
+
+const wizards =[
+    `Harry Potter`
+    `Hermione Granger`
+    `Ron Weasley`
+];
+
+for (list of wizards){
+    console.log(list)
+};
+
+console.log(`============`);
+
+let harryPotterMovies = 0;
+
+while(harryPotterMovies < 8){
+    console.log(harryPotterMovies); harryPotterMovies++;
+};
+
+console.log(harryPotterMovies);
